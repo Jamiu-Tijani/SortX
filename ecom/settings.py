57c75db195b19948aa82ff6551b9ccc411737d35
Jamiu-Tijani <jimniz01@gmail.com>
2021-03-03 17:27:20 +0100

    Error Commit

--- ecom/settings.py
+++ ecom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(qu+e=b)^m)xq-kv%nvh4ksb08*8%dxt110xd%!16kwrdea2r-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ecomscrap.herokuapp.com']
 
